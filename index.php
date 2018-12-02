98fb92b03ba637cf15749ecdaf2a2d35712f5b9b
KewinLizarraga <kewin392@gmail.com>
2018-12-02 01:04:39 -0500

    update

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        $ruta="location: view/index.php";
+        header($ruta);
         ?>
     </body>
 </html>
